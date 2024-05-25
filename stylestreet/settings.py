8b8a794e1e0538e7c3773a7d0a55b1b06eea788e
Abdul <abdul.ssyed@live.co.uk>
2024-05-25 21:38:30 +0500

    fixed setting

--- stylestreet/settings.py
+++ stylestreet/settings.py
@@ -205,7 +205,7 @@ if 'USE_AWS' in os.environ:
     }
     
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'gamer-on-board'
+    AWS_STORAGE_BUCKET_NAME = 'style-street'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
