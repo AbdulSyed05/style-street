15a580740cf880189f62bc99d7d1a57c574c71da
Abdul <abdul.ssyed@live.co.uk>
2024-05-25 19:06:07 +0500

    added settings

--- stylestreet/settings.py
+++ stylestreet/settings.py
@@ -16,7 +16,6 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +29,7 @@ SECRET_KEY = 'tl5f@@%^ycl#4+(0l#i=qqz#i7)u6@rg7uh3(heyc+_(ih#6'
 DEBUG = 'DEVELOPMENT' in os.environ
 DEBUG = False
 
-ALLOWED_HOSTS = ['*','stylestreet.herokuapp.com',]
+ALLOWED_HOSTS = ['*','stylestreet.herokuapp.com','localhost']
 
     
 
@@ -130,14 +129,10 @@ LOGOUT_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'stylestreet.wsgi.application'
 
-
 if 'DATABASE_URL' in os.environ:
-     DATABASES = {
-     'default': dj_database_url.parse('pip install dj_database_url==0.5.0 psycopg2')
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-    # DATABASES = {
-    #     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    # }
     print("Connected")
 else:
     DATABASES = {
@@ -148,22 +143,23 @@ else:
     }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    # },
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    # },
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
 
@@ -194,17 +190,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 
+
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
-        'CacheControl': 'max-age=86400',
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
     }
     
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'style-street'
+    AWS_STORAGE_BUCKET_NAME = 'gamer-on-board'
     AWS_S3_REGION_NAME = 'us-east-1'
-    AWS_ACCESS_KEY_ID = os.environ.get('AKIA6GBMEARNE64TBM4U')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('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')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
@@ -218,33 +216,48 @@ if 'USE_AWS' in os.environ:
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
     
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 100
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'eur'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51PJLAmBpRVF4CFnkEzJ9t5WhKZ5Hn9Gx7CIelvGMCEPx2Mh9RHAsaGInXrtkkOjtSKF60D0iqn39yV6XqKoQbKH000NUvecepc')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51PJLAmBpRVF4CFnkAKYREtnJGJSVzaLjV69keqmaRwFTy1s6hXHPwrZZwrMYK9Z4p05IiEUjoEuP20R5mJ6QT84P00oX8YAZv9')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'stylestreetecommerce@gmail.com'
 
+
 if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = 'stylestreetecommerce@gmail.com'
-    EMAIL_HOST_PASSWORD = 'gruv brad pohw oxmc'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'stylestreetecommerce@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = 'stylestreetecommerce@gmail.com'
-    EMAIL_HOST_PASSWORD = 'gruv brad pohw oxmc'
-    DEFAULT_FROM_EMAIL = 'stylestreetecommerce@gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+
+# if 'DEVELOPMENT' in os.environ:
+#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#     EMAIL_USE_TLS = True
+#     EMAIL_PORT = 587
+#     EMAIL_HOST = 'smtp.gmail.com'
+#     EMAIL_HOST_USER = 'stylestreetecommerce@gmail.com'
+#     EMAIL_HOST_PASSWORD = 'gruv brad pohw oxmc'
+#     DEFAULT_FROM_EMAIL = 'stylestreetecommerce@gmail.com'
+# else:
+#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#     EMAIL_USE_TLS = True
+#     EMAIL_PORT = 587
+#     EMAIL_HOST = 'smtp.gmail.com'
+#     EMAIL_HOST_USER = 'stylestreetecommerce@gmail.com'
+#     EMAIL_HOST_PASSWORD = 'gruv brad pohw oxmc'
+#     DEFAULT_FROM_EMAIL = 'stylestreetecommerce@gmail.com'
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
