58e08c4181a077d1318b09b1600dae6655848a21
Abdul Syed <138042476+AbdulSyed05@users.noreply.github.com>
2024-06-11 21:55:13 +0100

    Update models.py in products and made migrations for contact form

--- contact/models.py
+++ contact/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class ContactUs(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=False)
     email = models.EmailField()
-    message = models.TextField()
+    message = models.TextField(max_length=250, blank=False)
 
     def __str__(self):
         return self.email
--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Category(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = "Categories"
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
@@ -22,35 +22,37 @@ class Category(models.Model):
 class Condition(models.Model):
 
     class Meta:
-        verbose_name_plural = 'Condition'
+        verbose_name_plural = "Condition"
 
-    name = models.CharField(max_length=254)
+    name = models.CharField(max_length=254, help_text="", blank=False)
     condition_desc = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 
-
-  
-
-
 class Product(models.Model):
-      # Define Size Choices inside the Product model class
+    # Define Size Choices inside the Product model class
     SIZE_CHOICES = (
-        ('XS', 'Extra Small'),
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-        ('XL', 'Extra Large'),
+        ("XS", "Extra Small"),
+        ("S", "Small"),
+        ("M", "Medium"),
+        ("L", "Large"),
+        ("XL", "Extra Large"),
     )
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    name = models.CharField(max_length=254)
+    category = models.ForeignKey(
+        "Category", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    name = models.CharField(max_length=254, blank=False)
     color = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField()
     fabric = models.CharField(max_length=254)
-    size = models.CharField(max_length=2, choices=SIZE_CHOICES, default='M')  # Added size field with default value 'M'
-    condition = models.ForeignKey('Condition', null=True, blank=True, on_delete=models.SET_NULL)
+    size = models.CharField(
+        max_length=2, choices=SIZE_CHOICES, default="M"
+    )  # Added size field with default value 'M'
+    condition = models.ForeignKey(
+        "Condition", null=True, blank=True, on_delete=models.SET_NULL
+    )
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
@@ -59,9 +61,12 @@ class Product(models.Model):
     video = models.URLField(null=True, blank=True)
     stock = models.IntegerField(default=1)
     likes = models.ManyToManyField(User, related_name="product_likes", blank=True)
+    image1_alt_text = models.TextField(null=True, blank=False)
+    image2_alt_text = models.TextField(null=True, blank=False)
+    image3_alt_text = models.TextField(null=True, blank=False)
 
     def __str__(self):
         return self.name
-    
+
     def number_of_likes(self):
         return self.likes.count()
